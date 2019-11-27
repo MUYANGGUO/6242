e43e66d10a8191b86e2b30757d4672516593d4e1
MUYANG GUO <muyangguo@gmail.com>
2019-11-27 16:56:02 -0500

    pulled the click function update

--- Web-APP/public/scripts/sfpd.js
+++ Web-APP/public/scripts/sfpd.js
@@ -1,9 +1,15 @@
+
+
+
 function testsfpd(){
+    var lat = JSON.stringify(37.760);
+    var long = JSON.stringify(-122.38);
     $.ajax({
-        url: "https://data.sfgov.org/resource/wg3w-h783.json?incident_day_of_week=Friday",
+        url: "https://data.sfgov.org/resource/wg3w-h783.json?",
         type: "GET",
         data: {
-        "$limit" : 10,
+        "$where" : "within_circle(point,"+ lat +","+long+", 2000)",
+        "$limit" : 12,
         "$$app_token" : "m7cdlDr9n2gVd9FLts0mBiDyC"
         }
     }).done(function(data) {
